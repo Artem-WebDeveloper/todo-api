67bba09a07e98cbcc619d79e8388518835c1ecff
Artem-WebDeveloper <ave_art@icloud.com>
2026-07-27 22:36:28 +0300

    feat: add creating and deleting todo

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-
+const morgan = require('morgan');
 const todoRouter = require('./routes/todos.route');
 
 const PORT = process.env.PORT || 4000;
 
+app.use(morgan('dev'));
 app.use(express.json());
 app.use('/api/v1', todoRouter);
 
--- controllers/Todos.controller.js
+++ controllers/Todos.controller.js
@@ -46,9 +46,62 @@ class TodosController {
       });
     }
   }
-  async createTodo() {}
+  async createTodo(req, res) {
+    try {
+      const { text } = req.body;
+      if (typeof text !== 'string' || !text.trim()) {
+        return res.status(400).json({
+          status: 'fail',
+          message: 'Text is required and must be a non-empty string',
+        });
+      }
+
+      const cleanText = text.trim();
+
+      if (cleanText.length > 1000) {
+        return res.status(400).json({
+          status: 'fail',
+          message: `Text of todo exceeds 1000 symbols!`,
+        });
+      }
+
+      const result = await db.query('INSERT INTO todos (text) VALUES ($1) RETURNING *', [
+        cleanText,
+      ]);
+
+      res.status(201).json({
+        status: 'success',
+        data: result.rows[0],
+      });
+    } catch (err) {
+      res.status(500).json({
+        status: 'fail',
+        message: 'Something went wrong',
+      });
+    }
+  }
   async updateTodo() {}
-  async deleteTodo() {}
+
+  async deleteTodo(req, res) {
+    try {
+      const { id } = req.params;
+
+      const result = await db.query('DELETE FROM todos WHERE id = $1', [id]);
+      if (result.rowCount === 0) {
+        return res.status(404).json({
+          status: 'fail',
+          message: `Todo with id ${id} not found`,
+        });
+      }
+
+      res.status(204).send();
+    } catch {
+      res.status(500).json({
+        status: 'fail',
+        message: 'Something went wrong',
+      });
+    }
+  }
 }
 
 module.exports = new TodosController();
