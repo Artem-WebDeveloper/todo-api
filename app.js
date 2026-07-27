264b23ad41d4d3bae74c1178cb299741f2230ee7
Artem-WebDeveloper <ave_art@icloud.com>
2026-07-27 18:06:22 +0300

    feat: add postgres connection via pg pool

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
-
+const pool = require('./db');
 const port = 4000;
 
 app.get('/', (req, res) => {
@@ -10,3 +11,9 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Todo Server is running on port ${port}`);
 });
+
+(async function () {
+  console.log('DB connected');
+  const res = await pool.query('SELECT * FROM todos');
+  console.log(res.rows);
+})();
