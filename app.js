53d11d40b813f048dd0a2ddcf81966e64ccb8c12
Artem-WebDeveloper <ave_art@icloud.com>
2026-08-06 15:16:48 +0300

    feat: add error handler

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const todoRouter = require('./routes/todos.route');
+const globalErrorHandler = require('./controllers/error.controller');
 
 const PORT = process.env.PORT || 4000;
 
 app.use(morgan('dev'));
 app.use(express.json());
+
 app.use('/api/v1', todoRouter);
 
-app.use(err => {});
+app.all('/*splat', (req, res, next) => {
+  const err = new Error(`Can't find ${req.originalUrl} on this server!`);
+  err.status = 'fail';
+  err.statusCode = 404;
+  next(err);
+});
+
+app.use(globalErrorHandler);
 
 app.listen(PORT, () => {
   console.log(`Todo Server is running on port ${PORT}`);
